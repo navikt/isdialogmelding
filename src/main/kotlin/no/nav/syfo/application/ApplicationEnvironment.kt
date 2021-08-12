21949c62b985fc8e5e50c2aa0d5b43ac700f8f44
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-08-12 15:47:40 +0200

    Require envVars
    Fjern "allowNull"-metoden, siden vi nå krever at alt er på plass for at appen skal fungere.
    Flytt også på rekkefølgen, så MQ-variablene ligger i samme rekkefølge som i naiserator-filene

--- src/main/kotlin/no/nav/syfo/application/ApplicationEnvironment.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationEnvironment.kt
@@ -5,21 +5,18 @@ import io.ktor.application.*
 data class Environment(
     val aadAppClient: String = getEnvVar("AZURE_APP_CLIENT_ID"),
     val azureAppWellKnownUrl: String = getEnvVar("AZURE_APP_WELL_KNOWN_URL"),
-    val serviceuserUsername: String = getEnvVarAllowNull("SERVICEUSER_USERNAME"),
-    val serviceuserPassword: String = getEnvVarAllowNull("SERVICEUSER_PASSWORD"),
-    val mqChannelName: String = getEnvVarAllowNull("MQGATEWAY_CHANNEL_NAME"),
-    val mqHostname: String = getEnvVarAllowNull("MQGATEWAY_HOSTNAME"),
-    val mqQueueManager: String = getEnvVarAllowNull("MQGATEWAY_NAME"),
-    val mqPort: Int = getEnvVarAllowNull("MQGATEWAY_PORT", "1413").toInt(),
+    val serviceuserUsername: String = getEnvVar("SERVICEUSER_USERNAME"),
+    val serviceuserPassword: String = getEnvVar("SERVICEUSER_PASSWORD"),
+    val mqQueueManager: String = getEnvVar("MQGATEWAY_NAME"),
+    val mqHostname: String = getEnvVar("MQGATEWAY_HOSTNAME"),
+    val mqPort: Int = getEnvVar("MQGATEWAY_PORT", "1413").toInt(),
+    val mqChannelName: String = getEnvVar("MQGATEWAY_CHANNEL_NAME"),
+    val emottakQueuename: String = getEnvVar("MOTTAK_QUEUE_UTSENDING_QUEUENAME"),
     val mqApplicationName: String = "isdialogmelding",
-    val emottakQueuename: String = getEnvVarAllowNull("MOTTAK_QUEUE_UTSENDING_QUEUENAME"),
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
-
-fun getEnvVarAllowNull(varName: String, defaultValue: String? = null) =
-    System.getenv(varName) ?: defaultValue ?: ""
 
 val Application.envKind get() = environment.config.property("ktor.environment").getString()
 
