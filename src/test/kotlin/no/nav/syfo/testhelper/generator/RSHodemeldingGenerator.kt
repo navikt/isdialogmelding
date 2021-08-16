66045c9f6df1f111e5e19b05bb8ed6f32de7b5a0
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-08-16 16:14:51 +0200

    Add test for OppfolgingsplanService, verify sent message is correct
    "Fasit-meldingen" er hentet fra dialogfordeleren, bare endret noen navn, pluss ekstra escapes foran spørsmålstegn og paranteser, for at det skal fungere når man gjør om til Regex.
    Grunnen til at man bruker Regex, er for å alltid kunne matche på tid og uuid, som varierer mellom hver kjøring.

---
+++ src/test/kotlin/no/nav/syfo/testhelper/generator/RSHodemeldingGenerator.kt
@@ -0,0 +1,35 @@
+package no.nav.syfo.testhelper.generator
+
+import no.nav.syfo.oppfolgingsplan.domain.*
+
+fun generateRSHodemelding(): RSHodemelding {
+    return RSHodemelding(
+        meldingInfo = RSMeldingInfo(
+            mottaker = RSMottaker(
+                partnerId = "herId",
+                herId = "partnerId",
+                orgnummer = "orgnummer",
+                navn = "navn",
+                adresse = "adresse",
+                postnummer = "postnummer",
+                poststed = "poststed",
+                behandler = RSBehandler(
+                    fnr = "fnr",
+                    hprId = "hprId",
+                    fornavn = "Dana",
+                    mellomnavn = "Katherine",
+                    etternavn = "Scully",
+                )
+            ),
+            pasient = RSPasient(
+                fnr = "innbyggerFnr",
+                fornavn = "Idun",
+                mellomnavn = "mellomnavn",
+                etternavn = "Innbygger",
+            )
+        ),
+        vedlegg = RSVedlegg(
+            vedlegg = byteArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+        ),
+    )
+}
