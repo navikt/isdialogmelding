8e568a3f1f9e4c3c01581a9edd44ca32128a01be
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-11-29 17:32:25 +0100

    Bump dependencies to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ group = "no.nav.syfo"
 version = "1.0.0"
 
 object Versions {
-    const val flyway = "8.0.3"
+    const val flyway = "8.1.0"
     const val hikari = "5.0.0"
     const val jackson = "2.13.0"
     const val kafka = "2.8.0"
@@ -13,22 +13,22 @@ object Versions {
     const val ktor = "1.6.5"
     const val jaxb = "2.3.1"
     const val kluent = "1.68"
-    const val logback = "1.2.6"
-    const val logstashEncoder = "6.6"
-    const val mockk = "1.12.0"
-    const val nimbusjosejwt = "7.5.1"
+    const val logback = "1.2.7"
+    const val logstashEncoder = "7.0.1"
+    const val mockk = "1.12.1"
+    const val nimbusjosejwt = "9.15.2"
     const val spek = "2.0.17"
     const val syfotjenester = "1.2021.06.09-13.09-b3d30de9996e"
-    const val mq = "9.2.2.0"
-    const val micrometerRegistry = "1.7.5"
+    const val mq = "9.2.4.0"
+    const val micrometerRegistry = "1.8.0"
     const val postgres = "42.3.1"
     const val postgresEmbedded = "0.13.4"
 }
 
 plugins {
+    kotlin("jvm") version "1.6.0"
     id("com.github.johnrengelman.shadow") version "7.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
-    kotlin("jvm") version "1.5.31"
 }
 
 val githubUser: String by project
