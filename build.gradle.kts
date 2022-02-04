78ea48ebd7f85f9405f88fea93141816c88d60d5
Erik Jans <erik.gunnar.jansen@nav.no>
2022-02-04 11:39:40 +0100

    Bump dependency version to latest (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,12 @@ object Versions {
     const val logback = "1.2.10"
     const val logstashEncoder = "7.0.1"
     const val mockk = "1.12.2"
-    const val nimbusjosejwt = "9.15.2"
+    const val nimbusjosejwt = "9.18"
     const val spek = "2.0.17"
     const val syfotjenester = "1.2021.06.09-13.09-b3d30de9996e"
     const val mq = "9.2.4.0"
-    const val micrometerRegistry = "1.8.1"
-    const val postgres = "42.3.1"
+    const val micrometerRegistry = "1.8.2"
+    const val postgres = "42.3.2"
     const val postgresEmbedded = "0.13.4"
 }
 
