5746be0291e01bd419e4029d69c251605b6d151b
geir-waagboe <geir.arne.waagbo@nav.no>
2022-05-04 12:09:44 +0200

    Turn off processing of sykmeldinger

--- src/main/kotlin/no/nav/syfo/application/ApplicationUtils.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationUtils.kt
@@ -8,7 +8,6 @@ private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.application")
 
 fun launchBackgroundTask(
     applicationState: ApplicationState,
-    finallyNotReady: Boolean = true,
     action: suspend CoroutineScope.() -> Unit,
 ): Job = GlobalScope.launch {
     try {
@@ -16,11 +15,7 @@ fun launchBackgroundTask(
     } catch (ex: Exception) {
         log.error("Exception received while launching background task. Terminating application.", ex)
     } finally {
-        if (finallyNotReady) {
-            applicationState.alive = false
-            applicationState.ready = false
-        } else {
-            log.info("Background task exited normally")
-        }
+        applicationState.alive = false
+        applicationState.ready = false
     }
 }
