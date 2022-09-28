1f296b1d45dc7dd72558a40d4e49bbee4fb6027a
geir-waagboe <geir.arne.waagbo@nav.no>
2022-09-28 14:50:27 +0200

    Enable apprec-processing preprod

--- src/main/kotlin/no/nav/syfo/metric/Metrics.kt
+++ src/main/kotlin/no/nav/syfo/metric/Metrics.kt
@@ -9,7 +9,8 @@ const val METRICS_NS = "isdialogmelding"
 const val SEND_OPPFOLGINGSPLAN_SUCCESS = "${METRICS_NS}_send_opppfolgingsplan_success_count"
 const val SEND_OPPFOLGINGSPLAN_FAILED = "${METRICS_NS}_send_oppfolgingsplan_forbidden_count"
 const val SEND_MESSAGE_EMOTTAK_MQ = "${METRICS_NS}_send_message_emottak_mq_count"
-const val RECEIVED_APPREC = "${METRICS_NS}_received_apprec_emottak_mq_count"
+const val RECEIVED_APPREC_SUCCESSFUL = "${METRICS_NS}_received_apprec_emottak_mq_count"
+const val RECEIVED_APPREC_MESSAGE_QUEUE = "${METRICS_NS}_received_emottak_mq_count"
 
 val METRICS_REGISTRY = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
 
@@ -29,6 +30,11 @@ val COUNT_SEND_MESSAGE_EMOTTAK_MQ: Counter = Counter
     .register(METRICS_REGISTRY)
 
 val RECEIVED_APPREC_COUNTER: Counter = Counter
-    .builder(RECEIVED_APPREC)
+    .builder(RECEIVED_APPREC_SUCCESSFUL)
+    .description("Counts the number of received apprecs from mq")
+    .register(METRICS_REGISTRY)
+
+val RECEIVED_APPREC_MESSAGE_COUNTER: Counter = Counter
+    .builder(RECEIVED_APPREC_MESSAGE_QUEUE)
     .description("Counts the number of received apprecs from mq")
     .register(METRICS_REGISTRY)
