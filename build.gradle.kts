44ad1fbe6d493252e28bc7d6ee727025e995d0a9
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2023-04-03 20:00:15 +0200

    Upgrade gradle wrapper, add dependabot and codeql (#207)
    * Upgrade gradle wrapper, add dependabot and codeql
    
    * Use jvmToolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.apache.tools.ant.taskdefs.condition.Os
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
 version = "1.0.0"
@@ -148,6 +147,10 @@ dependencies {
     }
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 tasks {
     withType<Jar> {
         manifest.attributes["Main-Class"] = "no.nav.syfo.AppKt"
@@ -157,10 +160,6 @@ tasks {
         doLast {
             println(project.version)
         }
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
     }
 
     withType<ShadowJar> {
