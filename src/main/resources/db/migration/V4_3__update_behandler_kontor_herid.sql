e3ac61853f5160831a8b3609e70ce9bbfc2e5818
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-11-28 14:30:38 +0100

    Fix behandler kontor (#264)

---
+++ src/main/resources/db/migration/V4_3__update_behandler_kontor_herid.sql
@@ -0,0 +1,9 @@
+UPDATE BEHANDLER_KONTOR SET
+her_id='171911',
+orgnummer='927561867',
+navn='PSYKIATER SIGRID S. JOHNSEN',
+adresse='Starvhusgaten 2A',
+postnummer='5014',
+poststed='BERGEN',
+updated_at=now()
+WHERE ID=2226;
