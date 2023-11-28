5c5c3a4c28842150617f265e68ff6b9b6aa682d9
geir-waagboe <geir.arne.waagbo@nav.no>
2023-11-28 13:26:49 +0100

    IS-1857: Oppdaterer herId for behandler og kontor

---
+++ src/main/resources/db/migration/V4_3__update_behandler_kontor_herid.sql
@@ -0,0 +1,8 @@
+UPDATE BEHANDLER_KONTOR SET
+her_id='171911',
+navn='PSYKIATER SIGRID S. JOHNSEN',
+adresse='Starvhusgaten 2A',
+postnummer='5014',
+poststed='BERGEN',
+updated_at=now()
+WHERE ID=2226;
---
+++ src/main/resources/db/migration/V4_4__update_behandler_herid.sql
@@ -0,0 +1,1 @@
+UPDATE BEHANDLER SET her_id='164688',telefon='91303478',updated_at=now() WHERE id=9936;
