b30d36c1ae30bd9af93a727e0638192ea59a390e
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-12-21 14:08:05 +0100

    IS-1906: Disabler duplikate behandlerkontor, og legger inn manglende herId på noen behandlere (#282)

---
+++ src/main/resources/db/migration/V5_5__disable_behandler_kontor.sql
@@ -0,0 +1,9 @@
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=936;
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=209;
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=222;
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=1234;
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=503;
+UPDATE BEHANDLER SET her_id='159749' where id=23009;
+UPDATE BEHANDLER SET her_id='177974' where id=31244;
+UPDATE BEHANDLER SET her_id='178768' where id=33079;
+UPDATE BEHANDLER SET her_id='86655' where id=36753;
