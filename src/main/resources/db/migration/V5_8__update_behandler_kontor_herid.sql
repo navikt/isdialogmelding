a72f1d7bf47495cba6c696c9da9cd87aa0432c7b
geir-waagboe <geir.arne.waagbo@nav.no>
2024-02-01 15:45:10 +0100

    IS-1821: Kontor og behandlere har fått nye herid

---
+++ src/main/resources/db/migration/V5_8__update_behandler_kontor_herid.sql
@@ -0,0 +1,16 @@
+UPDATE BEHANDLER_KONTOR SET
+her_id='182102',
+updated_at=now()
+WHERE ID=2805;
+
+UPDATE BEHANDLER SET her_id='182531',updated_at=now() WHERE ID=20818 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182530',updated_at=now() WHERE ID=31114 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182535',updated_at=now() WHERE ID=35894 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182549',updated_at=now() WHERE ID=35937 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182539',updated_at=now() WHERE ID=35990 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182544',updated_at=now() WHERE ID=36110 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182548',updated_at=now() WHERE ID=36229 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182529',updated_at=now() WHERE ID=36574 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182536',updated_at=now() WHERE ID=36524 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182534',updated_at=now() WHERE ID=37119 AND kontor_id=2805;
+UPDATE BEHANDLER SET her_id='182527',updated_at=now() WHERE ID=37143 AND kontor_id=2805;
