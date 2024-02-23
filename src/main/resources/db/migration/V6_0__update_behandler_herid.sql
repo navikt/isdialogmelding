68087286fa7f975a51a4030cba0e37ad194dd0ae
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2024-02-23 12:10:53 +0100

    IS-2067: Update her-id for behandlere at kontor (#301)

---
+++ src/main/resources/db/migration/V6_0__update_behandler_herid.sql
@@ -0,0 +1,7 @@
+UPDATE BEHANDLER SET her_id='105186',updated_at=now() WHERE ID=20383 AND kontor_id=868;
+UPDATE BEHANDLER SET her_id='180028',updated_at=now() WHERE ID=33882 AND kontor_id=868;
+UPDATE BEHANDLER SET her_id='175449',updated_at=now() WHERE ID=25608 AND kontor_id=868;
+UPDATE BEHANDLER SET her_id='176644',updated_at=now() WHERE ID=28568 AND kontor_id=868;
+UPDATE BEHANDLER SET her_id='180064',updated_at=now() WHERE ID=33633 AND kontor_id=868;
+UPDATE BEHANDLER SET her_id='180006',updated_at=now() WHERE ID=33766 AND kontor_id=868;
+UPDATE BEHANDLER SET her_id='182585',updated_at=now() WHERE ID=37751 AND kontor_id=868;
