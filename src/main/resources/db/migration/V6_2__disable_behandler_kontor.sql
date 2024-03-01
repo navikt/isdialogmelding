f1892e5ff45bf748c72a935431209fd3c61a29dc
geir-waagboe <geir.arne.waagbo@nav.no>
2024-03-01 14:43:35 +0100

    IS-1946: Invalider kontor og tilhørende behandlere

--- src/main/resources/db/migration/V6_2__disable_behandler_kontor.sql
+++ src/main/resources/db/migration/V6_2__disable_behandler_kontor.sql
@@ -1,2 +1,5 @@
 UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null WHERE ID=1926;
 UPDATE BEHANDLER SET invalidated=now(),updated_at=now() WHERE kontor_id=1926;
+
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=1501;
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true WHERE ID=2103;
