7cfd6a50b29448434109a08ad4de5ee3182e658e
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-05-30 13:43:44 +0200

    IS-2416: Korrigere behandlerinformasjon (#320)

---
+++ src/main/resources/db/migration/V6_6__update_behandler_herid.sql
@@ -0,0 +1,6 @@
+UPDATE BEHANDLER SET her_id='177082',updated_at=now() WHERE behandler_ref='7ee9459e-485b-453a-af02-22ed7684bc17';
+UPDATE BEHANDLER SET her_id='176484',updated_at=now() WHERE behandler_ref='3017d6e7-02ef-4556-aef6-16cd110ffbc8';
+UPDATE BEHANDLER SET her_id='177081',updated_at=now() WHERE behandler_ref='e543285f-9673-49a9-8d1f-01bdd898cfdc';
+UPDATE BEHANDLER SET her_id='177080',updated_at=now() WHERE behandler_ref='1eb670e3-bbe3-44c3-9461-806082e05e37';
+UPDATE BEHANDLER SET her_id='183799',updated_at=now() WHERE behandler_ref='6617a50c-e10b-49e2-ab70-90fa65d76675';
+UPDATE BEHANDLER SET her_id='184032',updated_at=now() WHERE behandler_ref='6c386615-63f3-4277-a3eb-19f45f6b2134';
