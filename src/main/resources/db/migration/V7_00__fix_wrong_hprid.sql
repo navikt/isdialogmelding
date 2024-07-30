c39ee4237c0e384443ab524fabfc53717a0b156a
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-07-30 12:43:34 +0200

    IS-2404: rette opp flere hpr_id (#327)

---
+++ src/main/resources/db/migration/V7_00__fix_wrong_hprid.sql
@@ -0,0 +1,2 @@
+UPDATE BEHANDLER SET hpr_id='10085100' WHERE behandler_ref='1b01a001-5dd1-4066-996d-2db2ba70709c';
+UPDATE BEHANDLER SET hpr_id='10156975' WHERE behandler_ref='33a8eb03-c338-4d7f-a07c-8b1ca73f0ef3';
