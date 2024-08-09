9aaa638786b634487fb033e69b1056d399848ace
geir-waagboe <geir.arne.waagbo@nav.no>
2024-08-09 15:02:09 +0200

    IS-2581: Kjør cronjob ukentlig

--- src/main/kotlin/no/nav/syfo/cronjob/CronjobUtils.kt
+++ src/main/kotlin/no/nav/syfo/cronjob/CronjobUtils.kt
@@ -2,10 +2,7 @@ package no.nav.syfo.cronjob
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.time.Duration
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
+import java.time.*
 
 internal val log: Logger = LoggerFactory.getLogger("no.nav.syfo.cronjob.CronjobUtils")
 
@@ -14,6 +11,20 @@ fun calculateInitialDelay(cronJobName: String, runAtHour: Int): Long {
     val nowDate = LocalDate.now()
     val nextTimeToRun = LocalDateTime.of(
         if (from.hour < runAtHour) nowDate else nowDate.plusDays(1),
+        LocalTime.of(runAtHour, 0),
+    )
+    val initialDelay = Duration.between(from, nextTimeToRun).toMinutes()
+    log.info("$cronJobName will run in $initialDelay minutes at $nextTimeToRun")
+    return initialDelay
+}
+
+fun calculateWeeklyInitialDelay(cronJobName: String, runDay: DayOfWeek, runAtHour: Int): Long {
+    val from = LocalDateTime.now()
+    val nowDate = LocalDate.now()
+    val nowDay = nowDate.dayOfWeek
+    val daysUntilRunDay = if (nowDay <= runDay) (runDay.value - nowDay.value) else (7 - nowDay.value + runDay.value)
+    val nextTimeToRun = LocalDateTime.of(
+        nowDate.plusDays(if (daysUntilRunDay == 0 && runAtHour < from.hour) 7 else daysUntilRunDay.toLong()),
         LocalTime.of(runAtHour, 0),
     )
     val initialDelay = Duration.between(from, nextTimeToRun).toMinutes()
