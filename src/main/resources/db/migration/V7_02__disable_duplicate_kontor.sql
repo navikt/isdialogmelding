19e7e337a36e6c4d53ea220c8a7e8a783f28bdd6
geir-waagboe <geir.arne.waagbo@nav.no>
2024-08-13 13:13:49 +0200

    IS-2581: Disable duplikat behandlerkontor

---
+++ src/main/resources/db/migration/V7_02__disable_duplicate_kontor.sql
@@ -0,0 +1,2 @@
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true,updated_at=now() WHERE partner_id='29761';
+UPDATE BEHANDLER_KONTOR SET orgnummer='991790004',updated_at=now() where partner_id='29758';
