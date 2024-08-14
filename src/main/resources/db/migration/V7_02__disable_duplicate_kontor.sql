39d47cb47068758b337cd357a84a765ceb4191c2
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-08-14 15:23:53 +0200

    IS-2581: Disable duplikat behandlerkontor (#337)

---
+++ src/main/resources/db/migration/V7_02__disable_duplicate_kontor.sql
@@ -0,0 +1,3 @@
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true,updated_at=now() WHERE partner_id='29761';
+UPDATE BEHANDLER_KONTOR SET orgnummer='991790004',updated_at=now() where partner_id='29758';
+UPDATE BEHANDLER_KONTOR SET dialogmelding_enabled=null,dialogmelding_enabled_locked=true,updated_at=now() WHERE partner_id='53791';
