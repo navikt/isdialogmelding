a912feb8c0a4ba049f7f69f47f51d43e42233c97
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-11-27 15:25:17 +0100

    IS-2823: Add fysio kontor (#382)

---
+++ src/main/resources/db/migration/V7_11__add_kontor.sql
@@ -0,0 +1,2 @@
+INSERT INTO BEHANDLER_KONTOR(partner_id, her_id, navn, adresse, postnummer, poststed, orgnummer, dialogmelding_enabled, created_at, updated_at, system, mottatt, dialogmelding_enabled_locked)
+values('46803', '146997', 'FYSIOPARKEN DA', 'Sankt Hallvards vei 3', '3414', 'LIERSTRANDA', '921168187', now(), now(), now(), '', now(), false);
