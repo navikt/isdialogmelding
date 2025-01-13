77a459dd609725f125149ef4c07f2b6902ba49c2
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2025-01-13 15:55:49 +0100

    IS-3002: Aktiver behandlerkontor (#391)

--- src/main/kotlin/no/nav/syfo/cronjob/CronjobUtils.kt
+++ src/main/kotlin/no/nav/syfo/cronjob/CronjobUtils.kt
@@ -17,17 +17,3 @@ fun calculateInitialDelay(cronJobName: String, runAtHour: Int): Long {
     log.info("$cronJobName will run in $initialDelay minutes at $nextTimeToRun")
     return initialDelay
 }
-
-fun calculateWeeklyInitialDelay(cronJobName: String, runDay: DayOfWeek, runAtHour: Int): Long {
-    val from = LocalDateTime.now()
-    val nowDate = LocalDate.now()
-    val nowDay = nowDate.dayOfWeek
-    val daysUntilRunDay = if (nowDay <= runDay) (runDay.value - nowDay.value) else (7 - nowDay.value + runDay.value)
-    val nextTimeToRun = LocalDateTime.of(
-        nowDate.plusDays(if (daysUntilRunDay == 0 && runAtHour < from.hour) 7 else daysUntilRunDay.toLong()),
-        LocalTime.of(runAtHour, 0),
-    )
-    val initialDelay = Duration.between(from, nextTimeToRun).toMinutes()
-    log.info("$cronJobName will run in $initialDelay minutes at $nextTimeToRun")
-    return initialDelay
-}
