2d0b0c02f8d979ee796b361733a026ce816c06f4
eirikdahlen <dahleneirik@gmail.com>
2025-11-10 15:13:04 +0100

    IS-3518: Update behandlingsnr for PDL

--- src/main/kotlin/no/nav/syfo/client/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/syfo/client/pdl/PdlClient.kt
@@ -126,10 +126,8 @@ class PdlClient(
         private val logger = LoggerFactory.getLogger(PdlClient::class.java)
         const val IDENTER_HEADER = "identer"
 
-        // Se behandlingskatalog https://behandlingskatalog.intern.nav.no/
-        // Behandling: Sende, innhente og videreformidle informasjon mellom behandler og NAV. I sykefraværsoppfølgingen
-        // mottar NAV helseopplysninger fra behandler.
+        // Se behandlingskatalog https://behandlingskatalog.intern.nav.no/process/purpose/SYFO/60868e2d-b151-47a3-a1a6-1d896fc3a88d
         private const val BEHANDLINGSNUMMER_HEADER_KEY = "behandlingsnummer"
-        private const val BEHANDLINGSNUMMER_HEADER_VALUE = "B389"
+        private const val BEHANDLINGSNUMMER_HEADER_VALUE = "B426"
     }
 }
