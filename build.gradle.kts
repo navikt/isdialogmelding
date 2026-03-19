b32b8189bd6b904a8beec9e6f12cdd46f3471a57
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2026-03-19 13:07:24 +0100

    Bump dependencies (#496)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,26 +8,27 @@ val dialogmeldingVersion = "1.5d21db9"
 val fellesformat2Version = "1.0329dd1"
 val flywayVersion = "11.19.0"
 val hikariVersion = "7.0.2"
-val jacksonVersion = "2.20.1"
+val jacksonVersion = "2.21.1"
+val jacksonDatabindVersion = "3.1.0"
 val jaxbVersion = "2.3.1"
 val jsonVersion = "20250517"
-val kafkaVersion = "4.1.0"
+val kafkaVersion = "4.2.0"
 val kithApprecVersion = "2019.07.30-04-23-2a0d1388209441ec05d2e92a821eed4f796a3ae2"
 val kithHodemeldingVersion = "2019.07.30-12-26-5c924ef4f04022bbb850aaf299eb8e4464c1ca6a"
-val ktorVersion = "3.3.3"
-val logbackVersion = "1.5.22"
+val ktorVersion = "3.4.1"
+val logbackVersion = "1.5.32"
 val logstashEncoderVersion = "9.0"
-val micrometerRegistryVersion = "1.12.13"
-val mockkVersion = "1.14.7"
-val mqVersion = "9.4.4.0"
+val micrometerRegistryVersion = "1.16.3"
+val mockkVersion = "1.14.9"
+val mqVersion = "9.4.5.0"
 val nimbusjosejwtVersion = "10.6"
-val postgresVersion = "42.7.8"
+val postgresVersion = "42.7.10"
 val postgresEmbeddedVersion = "2.2.0"
 val postgresRuntimeVersion = "17.6.0"
 val syfotjenesterVersion = "1.2022.09.09-14.42-5356e2174b6c"
 
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.10"
     id("com.gradleup.shadow") version "8.3.8"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.adarshr.test-logger") version "4.0.0"
@@ -63,6 +64,7 @@ dependencies {
     implementation("io.micrometer:micrometer-registry-prometheus:$micrometerRegistryVersion")
 
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation("tools.jackson.core:jackson-databind:$jacksonDatabindVersion")
     implementation("javax.xml.bind:jaxb-api:$jaxbVersion")
     implementation("org.glassfish.jaxb:jaxb-runtime:$jaxbVersion")
 
@@ -85,28 +87,8 @@ dependencies {
         exclude(group = "org.apache.logging.log4j")
     }
     implementation("org.apache.kafka:kafka_2.13:$kafkaVersion", excludeLog4j)
-    constraints {
-        implementation("commons-beanutils:commons-beanutils") {
-            because("org.apache.kafka:kafka_2.13:$kafkaVersion -> https://www.cve.org/CVERecord?id=CVE-2025-48734")
-            version {
-                require("1.11.0")
-            }
-        }
-    }
     implementation("io.confluent:kafka-avro-serializer:$confluentVersion", excludeLog4j)
     constraints {
-        implementation("org.apache.avro:avro") {
-            because("org.apache.avro:avro:1.11.0 -> https://www.cve.org/CVERecord?id=CVE-2023-39410")
-            version {
-                require("1.11.4")
-            }
-        }
-        implementation("org.apache.commons:commons-compress") {
-            because("org.apache.commons:commons-compress:1.22 -> https://www.cve.org/CVERecord?id=CVE-2012-2098")
-            version {
-                require("1.28.0")
-            }
-        }
         implementation("com.google.guava:guava") {
             because("com.google.guava:guava:30.1.1-jre -> https://www.cve.org/CVERecord?id=CVE-2020-8908")
             version {
