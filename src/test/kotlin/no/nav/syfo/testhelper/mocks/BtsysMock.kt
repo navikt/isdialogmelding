bca8129aaf23321172cd56a65c1f17ed89b102a9
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-05-11 13:25:01 +0200

    IS-3941: Ta i bruk /suspensjon/soek i LegeSuspensjonClient

--- src/test/kotlin/no/nav/syfo/testhelper/mocks/BtsysMock.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/mocks/BtsysMock.kt
@@ -3,10 +3,10 @@ package no.nav.syfo.testhelper.mocks
 import io.ktor.client.engine.mock.*
 import io.ktor.client.request.*
 import no.nav.syfo.client.btsys.Suspendert
+import no.nav.syfo.client.btsys.SuspensjonSoekRequest
 import no.nav.syfo.testhelper.UserConstants
-import no.nav.syfo.util.NAV_PERSONIDENT_HEADER
 
-fun MockRequestHandleScope.btsysMockResponse(request: HttpRequestData): HttpResponseData {
-    val personIdentHeader = request.headers.get(NAV_PERSONIDENT_HEADER)
-    return respondOk(Suspendert(UserConstants.FASTLEGE_FNR_SUSPENDERT.value.equals(personIdentHeader)))
+suspend fun MockRequestHandleScope.btsysMockResponse(request: HttpRequestData): HttpResponseData {
+    val requestBody = request.receiveBody<SuspensjonSoekRequest>()
+    return respondOk(Suspendert(UserConstants.FASTLEGE_FNR_SUSPENDERT.value == requestBody.personident))
 }
